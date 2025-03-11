6fe28b93f7b9c94f31d03659ae9b5885205872be
Novita <novitaatwork@gmail.com>
2025-03-11 16:15:49 +0700

    create feature homepage

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://edot.id',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    defaultCommandTimeout: 10000, // set timeout for command 10 sec
+    pageLoadTimeout: 10000, // set timeout for loading page 10 sec
+    requestTimeout: 10000, // set timeout for HTTP request 10 sec
+    responseTimeout: 10000, // set timeout for HTTP response 10 sec
+    screenshotOnRunFailure: false, // non-activate automation screenshoot when failed
+    retries: {
+      runMode: 2, // set retest until 2x when fail in run mode
+      openMode: 1 // set retest 1x when fail in open mode
+    },
+    chromeWebSecurity: false // non-activate web chronme security
+  },
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,39 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Contact Us Form
+Cypress.Commands.add('contactUsForm', (user) => {
+    cy.get('#name').type(user.name)
+    cy.get('#email').type(user.email)
+    cy.get('#phone_number').type(user.phone_number)
+    cy.get('#company_name').type(user.company_name)
+    cy.get('#number_of_employees').click()
+    cy.get('div[data-value="51 - 100"]').click()
+    cy.get('#solutions').click()
+    cy.get('div[data-value="esuite (DMS)"]').click()
+    cy.get('#questions').type(user.questions)
+    cy.get('button[type="submit"]').click()
+})
